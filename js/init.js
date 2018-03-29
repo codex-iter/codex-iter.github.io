df5391070f17d2e3885fca564b27e1f7855f4318
A.Rupesh <a.rupesh66@gmail.com>
2018-03-29 19:20:17 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,88 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function() {
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('#intro-slider').flexslider({
+      animation: 'fade',
+      controlNav: false,
+   });
+
+/*----------------------------------------------------*/
+/*	gmaps
+------------------------------------------------------*/
+
+   var map;
+
+   // main directions
+   map = new GMaps({
+      el: '#map', lat: 20.2498709, lng: 85.8002307, zoom: 14, zoomControl : true,
+      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
+   });
+
+   // add address markers
+   map.addMarker({ lat: 20.2498709, lng: 85.8002307, title: 'ITER',
+   infoWindow: { content: '<p>You can add your address 1 here</p>' } });
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+
+      return false;
+
+   });
+
+
+});
+
+
+
+
+
+
+
+
