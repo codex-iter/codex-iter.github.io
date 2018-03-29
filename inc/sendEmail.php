4ddb7cb664a4396089480b98cee038ef34af9877
Zanark <kiodeba11@gmail.com>
2018-03-29 23:06:01 +0530

    Added newsletter and updated contact form(backend)

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,8 +1,10 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
-
+$siteOwnersEmail = 'codex.2.0.contact@gmail.com';
+			
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 if($_POST) {
 
@@ -28,7 +30,7 @@ if($_POST) {
 
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
+   $message = "Email from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
@@ -44,13 +46,58 @@ if($_POST) {
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
-   if (!$error) {
+   if (empty($error)) {
 
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+	  //$mail = mail($siteOwnersEmail, $subject, $message, $headers);
+	  
+	  	// Import PHPMailer classes into the global namespace
+		// These must be at the top of your script, not inside a function
 
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+
+
+
+			//Load Composer's autoloader
+			require '..\vendor\autoload.php';
+
+			//$email_id = $_POST['contactEmail'] ;
+
+			$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+			try {
+			    //Server settings
+			    //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			    $mail->isSMTP();                                      // Set mailer to use SMTP
+			    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+			    $mail->Username = 'codex.2.0.contact@gmail.com';                 // SMTP username
+			    $mail->Password = '';                                   // SMTP password [Password Changed and removed from GitHub for sequrity]
+			    //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			    $mail->Port = 25;                                    // TCP port to connect to
+			
+			    //Recipients
+			    $mail->setFrom('codex.2.0.contact@gmail.com', 'CODEX_ITER');
+			    $mail->addAddress($email, $name);     // Add a recipient
+			    $mail->addAddress('codex.2.0.contact@gmail.com');               // Name is optional
+			    //$mail->addReplyTo('info@example.com', 'Information');
+			    //$mail->addCC('cc@example.com');
+			    //$mail->addBCC('bcc@example.com');
+
+			    //Attachments
+			    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+			    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+			
+			    //Content
+			    $mail->isHTML(true);                                  // Set email format to HTML
+			    $mail->Subject = $subject;
+			    $mail->Body    = $message;
+			    $mail->AltBody = 'Thanks a ton for sending us a message. We were quite lonely here :D .';
+			
+			    $mail->send();
+			    echo 'Message has been sent';
+			} catch (Exception $e) {
+			    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+			}
+	
 		
 	} # end if - no validation error
 
@@ -65,5 +112,7 @@ if($_POST) {
 	} # end if - there was a validation error
 
 }
+
+
 
 ?>
