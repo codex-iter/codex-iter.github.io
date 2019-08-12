36f5addadc3cafccceb10c9a13569c04e1723ecb
Zanark <kiodeba11@gmail.com>
2019-08-12 17:50:50 +0530

    lets not push to jekyll branch first for now

--- jekyll-site-builder.py
+++ jekyll-site-builder.py
@@ -7,8 +7,8 @@ def jekyll_builder(cwd):
     #creates a ../temp folder
     #builds site inside that folder
 
-    os.system("git push origin jekyll")
-    print("\n * Pushed to jekyll branch" + "\n-------------------------\n-------------------------")
+    #os.system("git push origin jekyll")
+    #print("\n * Pushed to jekyll branch" + "\n-------------------------\n-------------------------")
 
     build_no = input("Enter the build number\t")
     comm_message = input("Describe your changes\t")
